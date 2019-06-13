9d66640c75c1c47159478ad81ffbdd8a5366dc13
ChentsovVS <32653400+ChentsovVS@users.noreply.github.com>
2019-06-13 15:09:59 +0300

    очищает таблицы предназначенные весовому товару
    truncate - путь до sql файла 
    backup - путь до бэкапа (на случай ядерной войны) 
    set_postgres_bin - в нашем случае - стандартная переменная до postgres/bin (из коробки PG_BIN)

---
+++ truncate_scale_for_Garchu.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+chcp 65001
+set truncate="C:\distrib\set script\cascade_scale_tables.sql"
+set backup=C:\backup_db\scale_reset\
+
+%set_postgres_bin%\pg_dump.exe -U postgres -Fc -d set > %backup%\set%date%.sql
+%set_postgres_bin%\psql.exe -U postgres -d set -f %truncate%
+%set_postgres_bin%\vacuumdb.exe -U postgres -f -z set
+echo теперь Вам необходимо выгрузить справочник весовых товаров из 1с (1с - номенклатура - весовой товар - выгрузить в SR10)
+
+timeout /t 10 
+
