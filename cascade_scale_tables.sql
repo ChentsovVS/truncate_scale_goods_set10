373ed6be198ec468680b8840ba3bcfd36d83acef
ChentsovVS <32653400+ChentsovVS@users.noreply.github.com>
2019-06-13 15:13:15 +0300

    очищает определенные таблицы в базе
    использует truncate cascade для очистки таблиц
    
    вызывается bat'ником

---
+++ cascade_scale_tables.sql
@@ -0,0 +1,11 @@
+truncate table 
+	scales_departproductentity,
+	scales_groupentity,
+	scales_linktoplu,
+	scales_plu_message_numbers,
+	scales_plues,
+	scales_productentity,
+	scales_productentity_barcodes,
+	scales_productentity_messages
+cascade
+; 
